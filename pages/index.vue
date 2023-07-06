b47faa218ff74a9070995641008b003b38bca67e
lukajaa <lucaskchang@gmail.com>
2023-07-06 11:26:25 -0700

    update:

--- pages/index.vue
+++ pages/index.vue
@@ -11,39 +11,21 @@
       </p>
     </div>
     <div class="flex min-h-screen flex-col items-center justify-center p-10">
-      <p
-        class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl"
-        data-aos="fade-in"
-        data-aos-delay="10"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-out"
-      >
+      <p class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl" data-aos="fade-in">
         <span class="bg-gradient-to-r from-red-500 via-pink-400 to-yellow-500 bg-clip-text text-transparent"
           >Trains are Cool</span
         >
       </p>
     </div>
     <div class="flex min-h-screen flex-col items-center justify-center p-10">
-      <p
-        class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl"
-        data-aos="fade-in"
-        data-aos-delay="10"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-out"
-      >
+      <p class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl" data-aos="fade-in">
         <span class="bg-gradient-to-r from-purple-500 via-blue-500 to-pink-500 bg-clip-text text-transparent"
           >So are Busses</span
         >
       </p>
     </div>
     <div class="flex min-h-screen flex-col items-center justify-center p-10">
-      <p
-        class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl"
-        data-aos="fade-in"
-        data-aos-delay="10"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-out"
-      >
+      <p class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl" data-aos="fade-in">
         So are
         <br />
         <span class="bg-gradient-to-r from-red-700 via-orange-500 to-yellow-500 bg-clip-text text-transparent"
@@ -54,13 +36,7 @@
       </p>
     </div>
     <div class="flex min-h-screen flex-col items-center justify-center p-10">
-      <p
-        class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl"
-        data-aos="fade-in"
-        data-aos-delay="10"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-out"
-      >
+      <p class="text-center text-4xl font-bold leading-loose tracking-tight md:text-8xl" data-aos="fade-in">
         <span class="bg-gradient-to-r from-teal-500 via-sky-500 to-cyan-500 bg-clip-text text-transparent"
           >Cars are Poopy</span
         >
--- plugins/aos.js
+++ plugins/aos.js
@@ -3,6 +3,10 @@ import 'aos/dist/aos.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (typeof window !== 'undefined') {
-    nuxtApp.AOS = AOS.init()
+    nuxtApp.AOS = AOS.init({
+      delay: 250,
+      duration: 500,
+      easing: 'ease-in-out'
+    })
   }
 })
